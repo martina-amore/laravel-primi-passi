eed5943f3d0a2bab88a4e0c161d483a9700f8a55
Martina Amore <martina.amore02@gmail.com>
2021-01-22 18:37:42 +0100

    Aggiunta rotte completa

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('titolo')
+    About
+@endsection
+
+@section('maincontent')
+    Martina Amore - Web Developer
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('titolo')</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('maincontent')
+        </div>
+    </body>
+</html>
